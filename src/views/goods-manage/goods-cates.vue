375bc2546a9b232b54ea8f07627093683118b300
lafeillou <lafeillou@126.com>
2019-05-05 12:09:02 +0800

    商品分类页面

--- src/views/goods-manage/goods-cates.vue
+++ src/views/goods-manage/goods-cates.vue
@@ -1,14 +1,34 @@
 <template>
-  <el-container class="goods-manage-wrap">
-    <el-main>
-      <div>
-        商品分类
-      </div>
-    </el-main>
-  </el-container>
+  <el-main class="contentwrapper">
+    <div>
+      <h3 class="heading">商品分类
+        <span style="float:right;margin-top:-3px;">
+          <router-link to="/goodsManage/index">
+            <el-button type="default" size="mini" style="">转移商品</el-button>
+          </router-link>
+          <router-link to="/goodsManage/index">
+            <el-button type="default" size="mini" style="">添加商品分类</el-button>
+          </router-link>
+        </span>
+      </h3>
+    </div>
+    <el-table
+      :data="tableData"
+      style="width:100%;"
+      header-row-class-name="py-table-header"
+      stripe>
+      <el-table-column v-for="item in tableSheme" :key="item.prop" :prop="item.prop" :label="item.name" :sortable="item.sortable" :width="item.width">
+        <template slot-scope="scope">
+          {{ scope.row[item.prop] }}
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-main>
 </template>
 
 <script>
+import tableSheme from './goods-cate-table-sheme'
+import tableDataForTest from './goods-cate-table-test-data'
 
 export default {
   name: 'GoodsCates',
@@ -16,7 +36,17 @@ export default {
 
   },
   data() {
-    return {}
+    return {
+      tableData: [
+
+      ],
+      tableSheme: tableSheme
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.tableData = tableDataForTest
+    }, 0)
   },
   methods: {}
 }
