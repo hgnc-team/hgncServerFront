32a48a489a9405f1fd2e179e2da65850a5faa409
lafeillou <lafeillou@126.com>
2019-05-07 20:29:42 +0800

    商品回收站页面制作

---
+++ src/views/goods-manage/goods-recycle-bin-table-sheme.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    prop: 'id',
+    name: '编号',
+    type: 'text',
+    editable: false,
+    sortable: false
+  },
+  {
+    prop: 'storeName',
+    name: '商家名称',
+    type: 'text',
+    editable: false,
+    sortable: false
+  },
+  {
+    prop: 'prodName',
+    name: '商品名称',
+    type: 'text',
+    editable: false,
+    sortable: false,
+    width: (document.body.clientWidth * 0.3 - 180)
+  },
+  {
+    prop: 'prodId',
+    name: '货号',
+    type: 'text',
+    editable: false,
+    sortable: false
+  },
+  {
+    prop: 'price',
+    name: '价格',
+    type: 'text',
+    editable: false,
+    sortable: false
+  }
+]
--- src/views/goods-manage/goods-recycle-bin.vue
+++ src/views/goods-manage/goods-recycle-bin.vue
@@ -68,11 +68,50 @@
           </el-col>
         </el-row>
       </el-form>
+      <el-table
+        :data="tableData"
+        style="width:100%;"
+        header-row-class-name="py-table-header"
+        stripe>
+        <el-table-column type="selection" />
+        <el-table-column v-for="item in tableSheme" :key="item.prop" :prop="item.prop" :label="item.name" :sortable="item.sortable" :width="item.width">
+          <template slot-scope="scope">
+            {{ scope.row[item.prop] }}
+          </template>
+        </el-table-column>
+        <el-table-column prop="operate" label="操作" width="90">
+          <template slot-scope="scope">
+            <div>
+              <el-button size="medium" type="text">
+                <font-awesome-icon :icon="['fas', 'share-square']" size="lg" fixed-width/>
+              </el-button>
+              <el-button size="medium" type="text">
+                <font-awesome-icon :icon="['fas', 'trash-alt']" size="lg" fixed-width class="py-text-danger"/>
+              </el-button>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <div class="pageNavWrap">
+        <el-pagination
+          :current-page="1"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="100"
+          :total="400"
+          layout="total, sizes, prev, pager, next, jumper"
+          background
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange" />
+      </div>
+
     </el-main>
   </el-container>
 </template>
 
 <script>
+import tableSheme from '@/views/goods-manage/goods-recycle-bin-table-sheme'
+import tableData from '@/views/goods-manage/goods-recycle-bin-table-test-data'
 
 export default {
   name: 'GoodsRecycleBin',
@@ -81,6 +120,10 @@ export default {
   },
   data() {
     return {
+      // 表头字段
+      tableSheme: tableSheme,
+      // 表格数据
+      tableData: tableData,
       preList: {
         batchOpList: [
           {
@@ -105,7 +148,18 @@ export default {
       }
     }
   },
-  methods: {}
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`)
+    },
+    handleCommand(command) {
+      console.log(command)
+      this.mainForm.batchOp = command
+    }
+  }
 }
 </script>
 
